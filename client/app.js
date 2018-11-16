8c4a6b1fc7583d9d6a65f0cbf5afa0f5b105d369
tanzj <843811863@qq.com>
2018-11-16 15:59:14 +0800

    optimize album component & add new dependencies

--- client/app.js
+++ client/app.js
@@ -4,13 +4,16 @@ import Header from '@/components/layout/header';
 import Center from '@/components/layout/center';
 import Home from '@/components/pages/home';
 
+// 引入 FontAwesome 字体库
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStroopwafel } from '@fortawesome/free-solid-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 import './css/common.css';
 
-library.add(faStroopwafel);
+// 引入全部 FontAwesome 字体
+library.add(fab, fas, far);
 
 class App extends React.Component {
     render() {
