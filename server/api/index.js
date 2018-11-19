7203619dd20017eda297558b97c4ccf8b8d0654b
邱雪 <1310265832@qq.com>
2018-11-19 18:02:00 +0800

    The backend interface takes effect

--- server/api/index.js
+++ server/api/index.js
@@ -1,28 +1,11 @@
-// import Koa from 'koa';
-// import Router from 'koa-router';
-// import Goods from './good';
-// const router = new Router();
-// import usersApi from './users';
-// import messagesApi from './messages';
-// import bookApi from './book';
-// import renewApi from './renews';
-// import bookdetailApi from './book-detail';
-// const router = express.Router();
-
-// console.log(Goods);
-// router.use('/good', Goods);
-// router.use('/users', usersApi);
-// router.use('/messages', messagesApi);
-// router.use('/users/current/books/borrowed', bookApi);
-// router.use('/users/books/renew', renewApi);
-// router.use('/users/current/books/book-detail', bookdetailApi);
-// export default router;
-
 const router = require('koa-router')();
+
 const user_router = require('./user_router');
+const music_router = require('./music');
+const good_router = require('./good');
 
 router.use('/users', user_router.routes(), user_router.allowedMethods());
+router.use('/music', music_router.routes(), music_router.allowedMethods());
+router.use('/good', good_router.routes(), good_router.allowedMethods());
 
 module.exports = router;
-
-// module.exports = router;
--- server/koa.server.js
+++ server/koa.server.js
@@ -1,10 +1,5 @@
-/**
- * koa2 server 入口
- */
-import db from './mongodb/db';
-import API from './api/index';
-
 const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
 const convert = require('koa-convert');
 const webpack = require('webpack');
 const merge = require('webpack-merge');
@@ -16,16 +11,19 @@ const proxyMiddleware = require('../config/middle/proxyMiddleWare');
 const spaMiddleWare = require('../config/middle/spaMiddleWare');
 const webpackDevMiddleware = require('koa-webpack-dev-middleware');
 const webpackHotMiddleware = require('koa-webpack-hot-middleware');
-
-const opn = require('opn');
 const config = merge(require('../config/webpack.config.dev'));
 const appConfig = require('../app.config');
 const currentIP = require('ip').address();
 const url = `http://${currentIP}:${appConfig.appPort}`;
+
+const db = require('./mongodb/db');
+
 const clientCompiler = webpack(config);
 const devMiddleware = webpackDevMiddleware(clientCompiler, {
     publicPath: config.output.publicPath,
-    headers: {'Access-Control-Allow-Origin': '*'},
+    headers: {
+        'Access-Control-Allow-Origin': '*'
+    },
     stats: {
         colors: true,
         modules: false,
@@ -35,7 +33,7 @@ const devMiddleware = webpackDevMiddleware(clientCompiler, {
 
 // koa server
 const app = new Koa();
-
+app.use(bodyParser());
 let router = KoaRouter;
 
 db.error((err) => {
@@ -45,55 +43,13 @@ db.connect(() => {
     console.log('数据库已连接');
 });
 
-// 装载所有路由
-const apiRouter = require('./api/index');
-app.use(apiRouter.routes());
-//
-// app.use('/api', apiRouter);
-
-// router.use('/goods', goods.routes());
-// router.use(router.routes());
-// router.use(router.allowedMethods());
-// 数据库接口
-// API.insertGoods();
-// API.search();
-// KoaRouter.get('/search', apiRouter.search);
-// router.get('/logout', Users.logout);
-
-// API.update();
-// API.delete();
-// console.log(API.search());
-// let monModel = require('./schema/good');
-//
-// router.get('/search', async (ctx, next) => {
-//     ctx.body = 'koa2 string';
-//
-//     // 查询条件
-//     let tj = {sname: '联想笔记本'};
-//
-//     // 查询商品内容信息
-//     let fields = {
-//         sname: 1,
-//         sprice: 2,
-//         stock: 3,
-//         addtime: 4,
-//         sinfo: 5
-//     };
-//
-//     monModel.find(tj, fields, function (err, data) {
-//         if (err) {
-//             console.log(err);
-//         } else {
-//             console.log(data);
-//             ctx.body = data;
-//         }
-//     });
-// });
-// app.use(router.routes());
-// app.use(router.allowedMethods());
+// 注册路由
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 const api = require('./api');
 router.use('/api', api.routes(), api.allowedMethods());
+
 // 中间件,一组async函数，generator函数需要convert转换
 const middleWares = [
     // 打印请求与响应 日志
@@ -117,12 +73,6 @@ const middleWares = [
 middleWares.forEach((middleware) => {
     if (!middleware) return;
     app.use(middleware);
-});
-
-// 项目启动打开浏览器页面
-devMiddleware.waitUntilValid(() => {
-    // console.log('> Listening at ' + url + '\n');
-    // opn(url)
 });
 
 // 错误处理
